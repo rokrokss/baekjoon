3e377a7801c44e26327860bee8c2d97415af005a
Hyungrok Kim <q0115643@kaist.ac.kr>
2018-03-18 20:24:07 +0900

    first commit

---
+++ 1475/1475.py
@@ -0,0 +1,17 @@
+N = input()
+N = int(N)
+arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+while True:
+    arr[N % 10] += 1
+    if N < 10:
+        break
+    N = int(N/10)
+num = 0
+for idx, n in enumerate(arr):
+    if idx != 9 and idx != 6:
+        if n > num:
+            num = n
+numSixNine = int((arr[6] + arr[9] + 1)/2)
+if numSixNine > num:
+    num = numSixNine
+print(num)
