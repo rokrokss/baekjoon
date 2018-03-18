ae999470211aecfd7ad7834931f54dbf0131d657
Hyungrok Kim <q0115643@kaist.ac.kr>
2018-03-19 00:27:01 +0900

    1181

--- 1181/1181.py
+++ 1181/1181.py
@@ -1,15 +1,11 @@
 import sys
 
 N = int(sys.stdin.readline())
-arr = [None] * 51
+arr = [set() for _ in range(51)]
 for i in range(N):
     w = sys.stdin.readline().rstrip()
     l = len(w)
-    if arr[l]:
-        arr[l].add(w)
-    else:
-        arr[l] = set()
-        arr[l].add(w)
+    arr[l].add(w)
 for s in arr:
     if s:
         for w in sorted(s):
