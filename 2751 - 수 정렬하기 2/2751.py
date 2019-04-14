bd9f3be1d21f8a450cabc47ba8275be28a2bca25
rokrokss <q0115643@kaist.ac.kr>
2019-04-15 00:58:11 +0900

    2751

--- 2751 - 수 정렬하기 2/2751.py
+++ 2751 - 수 정렬하기 2/2751.py
@@ -1,8 +1,32 @@
 from sys import stdin
+
+
+def build_heap(unsorted, index, heap_size):
+    largest = index
+    left_index, right_index = 2*index+1, 2*index+2
+    if left_index < heap_size and unsorted[left_index] > unsorted[largest]:
+        largest = left_index
+    if right_index < heap_size and unsorted[right_index] > unsorted[largest]:
+        largest = right_index
+    if largest != index:
+        unsorted[largest], unsorted[index] = unsorted[index], unsorted[largest]
+        build_heap(unsorted, largest, heap_size)
+
+
+def heapsort(arr):
+    n = len(arr)
+    for i in range(n//2-1, -1, -1):
+        build_heap(arr, i, n)
+    for i in range(n-1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        build_heap(arr, 0, i)
+
+
 read = stdin.readline
 t = int(read())
-input_list = []
+arr = []
 for _ in range(t):
-    n = int(read())
-    input_list.append(n)
-print(*sorted(input_list), sep='\n')
+    arr.append(int(read()))
+heapsort(arr)
+print(*arr, sep='\n')
+
