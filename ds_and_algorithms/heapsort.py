0f2d5dacc7f04bc6683e14e7696a09b98ec16446
rokrokss <q0115643@kaist.ac.kr>
2019-04-15 01:00:09 +0900

    heapsort fixed

--- ds_and_algorithms/heapsort.py
+++ ds_and_algorithms/heapsort.py
@@ -15,7 +15,7 @@ def build_heap(unsorted, index, heap_size):
 
 def heapsort(arr):
     n = len(arr)
-    for i in range(n//2-1, -1, -1):
+    for i in range((n-1)//2, -1, -1):
         build_heap(arr, i, n)
     for i in range(n-1, 0, -1):
         arr[0], arr[i] = arr[i], arr[0]
