709486bc1317837961f02a276bdbe61e810ef85d
rokrokss <q0115643@kaist.ac.kr>
2019-04-28 14:26:10 +0900

    1260 in es6

--- 1260 - DFS와 BFS/1260.js
+++ 1260 - DFS와 BFS/1260.js
@@ -1,61 +1,68 @@
-var Queue = function() {
-  this.head = null;
-  this.tail = null;
-};
-
-var Node = function(data) {
-  this.data = data;
-  this.next = null;
-};
-
-Queue.prototype.is_empty = function() {
-  return !this.head;
-};
-
-Queue.prototype.enqueue = function(data) {
-  var tmp = this.tail
-  this.tail = new Node(data);
-  if (this.is_empty()) {
-    this.head = this.tail;
-  } else {
-    tmp.next = this.tail;
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
-};
+}
 
-Queue.prototype.dequeue = function() {
-  var tmp = this.head;
-  this.head = tmp.next;
-  return tmp;
-};
 
-var PriorityQueue = function() {};
-PriorityQueue.prototype = Object.create(Queue.prototype);
-PriorityQueue.prototype.enqueue = function(data) {
-  var node = new Node(data);
-  if (this.is_empty()) {
-    this.head = node;
-  } else {
-    if (data < this.head.data) {
-      node.next = this.head;
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  is_empty() {
+    return !this.head;
+  }
+
+  enqueue(data) {
+    let tmp = this.tail
+    this.tail = new Node(data);
+    if (this.is_empty()) {
+      this.head = this.tail;
+    } else {
+      tmp.next = this.tail;
+    }
+  }
+
+  dequeue() {
+    let tmp = this.head;
+    this.head = tmp.next;
+    return tmp;
+  }
+}
+
+
+class PriorityQueue extends Queue {
+  enqueue(data) {
+    let node = new Node(data);
+    if (this.is_empty()) {
       this.head = node;
-      return;
-    }
-    var cur = this.head;
-    while (cur.next) {
-      if (cur.data < data && data < cur.next.data) {
-        node.next = cur.next;
-        break;
+    } else {
+      if (data < this.head.data) {
+        node.next = this.head;
+        this.head = node;
+        return;
       }
-      cur = cur.next;
+      let cur = this.head;
+      while (cur.next) {
+        if (cur.data < data && data < cur.next.data) {
+          node.next = cur.next;
+          break;
+        }
+        cur = cur.next;
+      }
+      cur.next = node;
     }
-    cur.next = node;
   }
-};
+}
 
-var dfs = function(v, adj, visited) {
+
+let dfs = (v, adj, visited) => {
   process.stdout.write((v + 1).toString() + ' ');
   visited[v] = true;
-  var cur = adj[v].head;
+  let cur = adj[v].head;
   while (cur) {
     if (!visited[cur.data]) {
       dfs(cur.data, adj, visited);
@@ -63,15 +70,14 @@ var dfs = function(v, adj, visited) {
     cur = cur.next;
   }
 };
-
-var bfs = function (v, adj, visited) {
-  var q = new Queue();
+let bfs = (v, adj, visited) => {
+  let q = new Queue();
   q.enqueue(v);
   visited[v] = true;
   while (!q.is_empty()) {
-    var u = q.dequeue().data;
+    let u = q.dequeue().data;
     process.stdout.write((u + 1).toString() + ' ');
-    var cur = adj[u].head;
+    let cur = adj[u].head;
     while (cur) {
       if (!visited[cur.data]) {
         q.enqueue(cur.data);
@@ -81,20 +87,18 @@ var bfs = function (v, adj, visited) {
     }
   }
 };
-
-var input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-var nmv = input[0].split(' ').map(function(x) {return Number(x)});
-var n = nmv[0], m = nmv[1], v = nmv[2] - 1;
-var adj = [];
-var visited = [];
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let [n, m, v] = input[0].split(' ').map(x => {return Number(x)}); v--;
+let adj = Array(n);
+let visited = Array(n);
 for (var i = 0; i < n; i++) {
-  adj.push(new PriorityQueue());
+  adj[i] = new PriorityQueue();
   visited[i] = false;
 }
-for (var i = 1; i <= m; i++) {
-  var ab = input[i].split(' ').map(function(x) {return Number(x) - 1});
-  adj[ab[0]].enqueue(ab[1]);
-  adj[ab[1]].enqueue(ab[0]);
+for (let i = 1; i <= m; i++) {
+  let [a, b] = input[i].split(' ').map(x => {return Number(x) - 1});
+  adj[a].enqueue(b);
+  adj[b].enqueue(a);
 }
 dfs(v, adj, visited);
 process.stdout.write('\n');
