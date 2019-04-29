840fa8f9af0813fb9b4787611911ddc9928d4048
rokrokss <q0115643@kaist.ac.kr>
2019-04-29 20:53:48 +0900

    fix quicksort

--- ds_and_algorithms/quicksort.js
+++ ds_and_algorithms/quicksort.js
@@ -4,23 +4,22 @@ function randomInt(min, max) {
 
 
 function _quickSort(arr, start, end) {
-  if (start === end) {
-    return arr;
-  } else {
-    let pivot_idx = randomInt(start, end);
-    let pivot_val = arr[pivot_idx];
-    [arr[pivot_idx], arr[end]] = [arr[end], arr[pivot_idx]];
-    store_idx = start;
-    for (let i = start; i < end; i++) {
-      if (arr[i] < pivot_val) {
-        [arr[store_idx], arr[i]] = [arr[i], arr[store_idx]];
-        store_idx++;
-      }
-    }
-    [arr[store_idx], arr[end]] = [arr[end], arr[store_idx]];
-    _quickSort(arr, start, store_idx - 1);
-    _quickSort(arr, store_idx + 1, end);
+  if (start >= end) {
+    return;
   }
+  let pivot_idx = randomInt(start, end);
+  let pivot_val = arr[pivot_idx];
+  [arr[pivot_idx], arr[end]] = [arr[end], arr[pivot_idx]];
+  let store_idx = start;
+  for (let i = start; i < end; i++) {
+    if (arr[i] < pivot_val) {
+      [arr[store_idx], arr[i]] = [arr[i], arr[store_idx]];
+      store_idx++;
+    }
+  }
+  [arr[store_idx], arr[end]] = [arr[end], arr[store_idx]];
+  _quickSort(arr, start, store_idx - 1);
+  _quickSort(arr, store_idx + 1, end);
 }
 
 
