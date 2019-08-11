0c7cd013409fa74c460256b90ff6645801a1af67
q0115643 <q0115643@kaist.ac.kr>
2019-08-12 00:43:58 +0900

    2869

--- 2869 - 달팽이는 올라가고 싶다/2869.kt
+++ 2869 - 달팽이는 올라가고 싶다/2869.kt
@@ -1,10 +1,8 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-fun f(n: Int): Int = if (n <= 1) 1 else n * f(n - 1)
-
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
-    val n = br.readLine().toInt()
-    println(f(n))
+    val (a, b, v) = br.readLine().split(" ").map { it.toInt() }
+    println((v - b - 1)/(a - b) + 1)
 }
