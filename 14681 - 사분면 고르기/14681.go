4ce6ef3283fcf9748c569be06ba30b021e0b44f5
rokrokss <q0115643@gmail.com>
2020-04-09 01:16:12 +0900

    14681 go

---
+++ 14681 - 사분면 고르기/14681.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x, y int
+	fmt.Scan(&x, &y)
+	if x > 0 {
+		if y > 0 {
+			fmt.Println(1)
+		} else {
+			fmt.Println(4)
+		}
+	} else {
+		if y > 0 {
+			fmt.Println(2)
+		} else {
+			fmt.Println(3)
+		}
+	}
+}
